152c4e669a029a8d2c0cb8676ec949790855c002
JackDUpModZ <83776161+JackDUpModZ@users.noreply.github.com>
2021-11-20 14:54:20 +0000

    Update server.lua

--- JD_Evidence/server/server.lua
+++ JD_Evidence/server/server.lua
@@ -135,3 +135,25 @@ ESX.RegisterServerCallback('JD_Evidence:getPlayerName', function(source,cb)
       end
   end)
 end)
+
+
+Citizen.CreateThread( function()
+	updatePath = "/JackDUpModZ/JD_Evidence" -- your git user/repo path
+	resourceName = "JD_Evidence ("..GetCurrentResourceName()..")" -- the resource name
+	
+	function checkVersion(err,responseText, headers)
+		curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
+	
+		if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+			print("\n#############################################################\n")
+			print(""..resourceName.." is outdated!\n\nLatest Available!\n \nVersion : "..responseText.."\nCurrent Resource\n\nVersion : "..curVersion.."\nplease update it from https://github.com"..updatePath.."")
+			print("\n#############################################################\n")
+		elseif tonumber(curVersion) > tonumber(responseText) then
+			print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
+		else
+			print(""..resourceName.." is up to date, have fun!")
+		end
+	end
+	
+	PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
+end)
